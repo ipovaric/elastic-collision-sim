3f1a2005eb9b7251c4da62a7b91b44e36eb1c549
Igor <ipovaric@gmail.com>
2026-09-05 09:42:33 -0600

    feat: collision with 3rd particle
    Still referenced to just A. Going to need a more robust collision
    resolution to work with many more particles.

--- multi_sphere.py
+++ multi_sphere.py
@@ -6,25 +6,40 @@ RC = 0.01
 bA = sphere(pos=vector(-8*RA,0.5*RA,0),radius=RA,color=color.yellow,make_trail=True)
 bB = sphere(pos=vector(0,0,0),radius=RB,color=color.cyan,make_trail=True)
 bC = sphere(pos=vector(0,5*RC,0),radius=RC,color=color.red,make_trail=True)
-bA.m = 4
+bA.m = 3
 bB.m = 1
+bC.m = 2
 v0 = 0.05
-bA.p = bA.m*vector(v0,0,0)
+v1 = 0.01
+bA.p = bA.m*vector(0.5*v0,0,0)
 bB.p = bB.m*vector(0,0,0)
+bC.p = bC.m*vector(0,-v1,0)
 
 t = 0
-dt = 0.01
+dt = 0.02
 k = 500
-while t < 3:
+while t < 7:
     rate(100)
-    rAB = bA.pos - bB.pos
     F = vector(0,0,0)
+    FAB = vector(0,0,0)
+    FAC = vector(0,0,0)
+    rAB = bA.pos - bB.pos
+    rAC = bA.pos - bC.pos
+    
     # check A collision w/ B
     if mag(rAB) < (RA+RB):      
-        F = k*((RA+RB)-mag(rAB))*norm(rAB)
+        FAB = k*((RA+RB)-mag(rAB))*norm(rAB)
+        F = FAB
         print(F)
+    # check A collision w/ C
+    if mag(rAC) < (RA+RC):      
+        FAC = k*((RA+RC)-mag(rAC))*norm(rAC)
+        F = FAC
+        print(FAC)
     bA.p = bA.p + F*dt
-    bB.p = bB.p - F*dt
+    bB.p = bB.p - FAB*dt
+    bC.p = bC.p - FAC*dt
     bA.pos = bA.pos + bA.p*dt/bA.m
     bB.pos = bB.pos + bB.p*dt/bB.m
+    bC.pos = bC.pos + bC.p*dt/bC.m
     t = t + dt
